63a554ef1a7f57857378f79267756dc8da611546
Sofia Enriquez <lsofia.enriquez@gmail.com>
2023-04-19 11:30:13 +0100

    Rich and os env added to the project

--- README.md
+++ README.md
@@ -1,46 +1,27 @@
 # Launchpad Bug Report 
 
-:tea: This is a simple script that generates a list of bugs from today minus 7 days. However, you can easily change this. 
-
-## Sample
-
-```
-(.env)$ python main.py 
-cinder
----
-- Bug #1971483
- Date 2022-05-03 19:33:33.877298+00:00
- Link https://bugs.launchpad.net/cinder/+bug/1971483
-
-os-brick
----
-No bugs from 2022-04-30 to 2022-05-10
-
-cinderlib
----
-No bugs from 2022-04-30 to 2022-05-10
-
-cinder-tempest-plugin
----
-No bugs from 2022-04-30 to 2022-05-10
-```
+![Sample of the script output](sample.png)
 
 
-## Getting Started
+## Description
+:coffee: Script to generate a list of bugs from today minus the days you want :coffee
 
-### Installation
+## Quick Start
+
+### Setup virtual environment
 ```
 python3 -m venv myenv
+source myenv/bin/activate
 pip install -r requirements.txt
+```
+
+### Run the script
+
+```
+cp env.sample .env
 python main.py
 ```
 
-### Usage
-The Launchpad client requires UTC time. So if you want to generate a report from a different date, you can do so like this:
-
-```
-utc_start_date = datetime(2022, 4, 27).astimezone(tz.UTC)
-```
 
 ## Contributing
 Contributions are what make the open source community such a great place for learning, inspiration and creativity. Any contribution you make is greatly appreciated.
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 #! /usr/bin/python3
+import os
 from datetime import datetime, timedelta, timezone
 
 from launchpadlib.launchpad import Launchpad as LP
 
-LP_PROJECTS = ["cinder", "os-brick", "cinderlib", "cinder-tempest-plugin"]
-LP_PROJECT = "cinder"
-LP_ENV = "production"  # or 'staging'
-LP_LINK = "https://bugs.launchpad.net/%s/+bug/%d"
-LP_REASON = "testing"
-LP_VERSION = "devel"
+from rich.console import Console
+from rich.table import Table
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LP_PROJECTS = os.getenv("LP_PROJECTS", "cinder").split(",")
+LP_PROJECT = os.getenv("LP_PROJECT", "cinder")
+LP_ENV = os.getenv("LP_ENV", "production")  # or 'staging'
+LP_LINK = os.getenv("LP_LINK", "https://bugs.launchpad.net/%s/+bug/%d")
+LP_REASON = os.getenv("LP_REASON", "testing")
+LP_VERSION = os.getenv("LP_VERSION", "devel")
 
 # These filter the types of bugs we want (i.e. anything open)
 STATUSES = [
@@ -29,41 +37,86 @@ IMPORTANCE = [
     "Wishlist",
 ]
 
-# The documents you retrieve from Launchpad will be stored here, which
-# will save you a lot of time.
-cachedir = "~/launchpad-bug-reporter/.launchpadlib/cache/"
+
+def _create_table(project_name, start_date, end_date):
+    table = Table(
+        title=f"{project_name.capitalize()} Bugs from {start_date:%Y-%m-%d} to {end_date:%Y-%m-%d}"
+    )
+    table.add_column("Date", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Bug #", justify="right", style="magenta")
+    table.add_column("Importance", style="magenta")
+    table.add_column("Title", style="magenta")
+    table.add_column("Status", style="magenta")
+    table.add_column("Assignee", style="magenta")
+    table.add_column("Link", style="green")
+    return table
 
 
 def main():
-    days_to_subtract = 10
+    # The documents you retrieve from Launchpad will be stored here, which
+    # will save you a lot of time.
+    # get path to this directory
+    path_dir = os.path.dirname(os.path.realpath(__file__))
+    cachedir = os.getenv("LP_CACHE_DIR", f"{path_dir}/.launchpadlib/cache/")
+    print(f"Using cache dir: {cachedir}")
+
+    if not os.path.exists(cachedir):
+        os.makedirs(cachedir)
+    days_to_subtract = int(os.getenv("DAYS_TO_SUBTRACT", 7))
     utc_end_date = datetime.now(tz=timezone.utc)
     utc_start_date = utc_end_date - timedelta(days=days_to_subtract)
 
     # Anonymous and read-only access to public Launchpad data
     lp = LP.login_anonymously(LP_REASON, LP_ENV, cachedir, version=LP_VERSION)
 
-    print("Bug Report")
-    print(f"from {utc_start_date:%Y-%m-%d} to {utc_end_date:%Y-%m-%d}")
-    print(f"with IMPORTANCE={IMPORTANCE[0]}")
-
     for lp_project in LP_PROJECTS:
-        print(f"\n{lp_project}\n---")
+        # Rich table
+        table = _create_table(lp_project, utc_start_date, utc_end_date)
+
+        # Launchpad
         project = lp.projects[lp_project]
 
         tasks = project.searchTasks(
             created_since=utc_start_date,
             created_before=utc_end_date,
-            importance=IMPORTANCE[0],
+            # importance=IMPORTANCE[0],
         )
 
         if len(tasks) == 0:
-            print("No bugs")
+            table.add_row(
+                "No bugs found",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+            )
+
         else:
             for task in tasks:
                 bug = task.bug
                 link = LP_LINK % (lp_project, bug.id)
-                print(f"Bug #{bug.id} - {bug.title}")
-                print(f"Date {bug.date_created:%Y-%m-%d}\nLink {link}\n")
+
+                assignee_status = "Unassigned"
+                if task.assignee:
+                    assignee_status = "Yes"
+
+                bug_date = bug.date_created.strftime("%Y-%m-%d")
+                bug_id = f"#{bug.id}"
+
+                table.add_row(
+                    bug_date,
+                    bug_id,
+                    task.importance,
+                    bug.title,
+                    task.status,
+                    assignee_status,
+                    link,
+                )
+
+        console = Console()
+        console.print(table)
 
 
 if __name__ == "__main__":
